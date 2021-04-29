f85a998d5addf70f3b0d2f29b42130d4fcb7fd82
Davide Stefania <davide16797@gmail.com>
2021-04-29 10:00:51 +0200

    evaluate() and evaluateParentheses() functions

--- code.js
+++ code.js
@@ -12,8 +12,9 @@ function divide(a, b) {
   return b === 0 ? "ERROR" : a / b; //return "ERROR" if you divide by 0;
 }
 
+//takes an array like ["1", "+", "1", "-", "3"], the index of the operation (1), and the operation to do ("+")
+//returns an array with the result of the operation ["2", "-", "3"]
 function doOperation(array, indexOfOperation, operation) {
-  //[1, +, 2]
   let first = array[indexOfOperation - 1];
   let second = array[indexOfOperation + 1];
   let result;
@@ -37,5 +38,64 @@ function doOperation(array, indexOfOperation, operation) {
   return array;
 }
 
-let testArray = ["4", "+", "7"];
-console.log(doOperation(testArray, 1, "+"));
+//takes an array of operations without parentheses
+//returns the results of the operations
+function evaluate(array) {
+  let currentOperation;
+  while (array.indexOf("*") > 0) {
+    currentOperation = "*";
+    array = doOperation(
+      array,
+      array.indexOf(currentOperation),
+      currentOperation
+    );
+  }
+  while (array.indexOf("/") > 0) {
+    currentOperation = "/";
+    array = doOperation(
+      array,
+      array.indexOf(currentOperation),
+      currentOperation
+    );
+  }
+  while (array.indexOf("+") > 0) {
+    currentOperation = "+";
+    array = doOperation(
+      array,
+      array.indexOf(currentOperation),
+      currentOperation
+    );
+  }
+  while (array.indexOf("-") > 0) {
+    currentOperation = "-";
+    array = doOperation(
+      array,
+      array.indexOf(currentOperation),
+      currentOperation
+    );
+  }
+  return array[0];
+}
+
+function evaluateParentheses(array) {
+  let parOpenIndex;
+  let parCloseIndex;
+  let parResult;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "(") {
+      parOpenIndex = i;
+    } else if (array[i] === ")") {
+      parCloseIndex = i;
+      parResult = evaluate(
+        array.splice(parOpenIndex + 1, parCloseIndex - parOpenIndex - 1)
+      );
+      array.splice(parOpenIndex, 2, parResult);
+      return array;
+    }
+    continue;
+  }
+}
+
+let testArray = ["(", "4", "+", "2", ")", "*", "30", "/", "3"];
+console.log(evaluate(evaluateParentheses(testArray)));
